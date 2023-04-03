701f2b91e23c4123363d3dd9916b1b02912e36ec
William TRAN <tran-william@hotmail.fr>
2023-04-03 22:22:22 +0200

    Initialize Application and Launcher

---
+++ launcher/src/main/java/fr/william/pdfgenerator/launcher/PDFGeneratorLauncher.java
@@ -0,0 +1,11 @@
+package fr.william.pdfgenerator.launcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PDFGeneratorLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(PDFGeneratorLauncher.class, args);
+    }
+}
